ef3ced2798cf3f264aa661b355ebb3596a228fda
Alibek10599 <akhojabekov@gmail.com>
2024-12-13 17:42:34 +0500

    remove comments

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -28,14 +28,12 @@ contract ZombieFeeding is ZombieFactory {
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    // Add an if statement here
     _createZombie("NoName", newDna);
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    // And modify function call here:
     feedAndMultiply(_zombieId, kittyDna);
   }
 
